e2d9a0aee999f9b481cb1db9cb4e13a64cc6e9ea
r-terada <r.terada1993@gmail.com>
2019-08-04 16:43:01 +0900

    fix metrics to multi label

--- data_utils/metrics.py
+++ data_utils/metrics.py
@@ -11,6 +11,12 @@ def compute_acc(predicts, labels):
 def compute_f1(predicts, labels):
     return 100.0 * f1_score(labels, predicts)
 
+def compute_micro_f1(predicts, labels):
+    return 100.0 * f1_score(labels, predicts, average='micro')
+
+def compute_macro_f1(predicts, labels):
+    return 100.0 * f1_score(labels, predicts, average='macro')
+
 def compute_mcc(predicts, labels):
     return 100.0 * matthews_corrcoef(labels, predicts)
 
@@ -25,9 +31,20 @@ def compute_spearman(predicts, labels):
 def compute_precision(predicts, labels):
     return 100.0 * precision_score(labels, predicts)
 
+def compute_micro_precision(predicts, labels):
+    return 100.0 * precision_score(labels, predicts, average='micro')
+
+def compute_macro_precision(predicts, labels):
+    return 100.0 * precision_score(labels, predicts, average='macro')
+
 def compute_recall(predicts, labels):
     return 100.0 * recall_score(labels, predicts)
 
+def compute_micro_recall(predicts, labels):
+    return 100.0 * recall_score(labels, predicts, average='micro')
+
+def compute_macro_recall(predicts, labels):
+    return 100.0 * recall_score(labels, predicts, average='macro')
 
 class Metric(Enum):
     ACC = 0
@@ -37,6 +54,12 @@ class Metric(Enum):
     Spearman = 4
     Precision = 5
     Recall = 6
+    MICRO_F1 = 7
+    MACRO_F1 = 8
+    MICRO_Precision = 9
+    MACRO_Precision = 10
+    MICRO_Recall = 11
+    MACRO_Recall = 12
 
 
 METRIC_FUNC = {
@@ -47,4 +70,10 @@ METRIC_FUNC = {
  Metric.Spearman: compute_spearman,
  Metric.Precision: compute_precision,
  Metric.Recall: compute_recall,
+ Metric.MICRO_F1: compute_micro_f1,
+ Metric.MACRO_F1: compute_macro_f1,
+ Metric.MICRO_Precision: compute_micro_precision,
+ Metric.MACRO_Precision: compute_macro_precision,
+ Metric.MICRO_Recall: compute_micro_recall,
+ Metric.MACRO_Recall: compute_macro_recall,
 }
