70e9a4a925253b312167b54a629764ec6c0a5cd7
r-terada <r.terada1993@gmail.com>
2019-08-18 19:14:49 +0900

    add prepro script for ner all class

--- experiments/japanese/bccwj_prepro.py
+++ experiments/japanese/bccwj_prepro.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import random
 import argparse
@@ -7,7 +8,7 @@ path.append(os.getcwd())
 from pytorch_pretrained_bert.tokenization import BertTokenizer
 
 from data_utils.log_wrapper import create_logger
-from experiments.japanese.bccwj_label_map import NERLabelMapper, POSLabelMapper
+from experiments.japanese.bccwj_label_map import NERLabelMapper, NERENELabelMapper, POSLabelMapper
 
 logger = create_logger(__name__, to_disk=True, log_file='glue_prepro.log')
 
@@ -17,6 +18,7 @@ def parse_args():
     parser.add_argument('--seed', type=int, default=13)
     parser.add_argument('--data_dir', type=str, default='data')
     parser.add_argument('--bert_model', type=str, default='mt_dnn_models/vocab.txt')
+    parser.add_argument('--tasks', type=str, default='ner,pos')
     parser.add_argument('--max_seq_len', type=int, default=128)
     args = parser.parse_args()
     return args
@@ -24,6 +26,33 @@ def parse_args():
 
 def load_ner(file, eos='。'):
     """Loading data of bccwj with ner labels
+    """
+    rows = []
+    cnt = 0
+    with open(file, encoding="utf8") as f:
+        words = []
+        labels = []
+        for line in f:
+            contents = line.strip()
+            if contents.startswith("-DOCSTART-") or len(contents) == 0:
+                    continue
+
+            if len(words) > 0 and words[-1] == eos:
+                sample = {'uid': str(cnt), 'premise': words, 'label': labels}
+                rows.append(sample)
+                cnt += 1
+                words = []
+                labels = []
+            else:
+                word = contents.split(' ')[0]
+                label = contents.split(' ')[-1]
+                words.append(word)
+                labels.append(label)
+    return rows
+
+
+def load_ner_ene(file, eos='。'):
+    """Loading data of bccwj with all ner labels
     """
     rows = []
     cnt = 0
@@ -112,6 +141,11 @@ def main(args):
     root = args.data_dir
     assert os.path.exists(root)
 
+    tasks = args.tasks.split(',')
+    for t in tasks:
+        if not t in ['ner', 'pos', 'ner_ene']:
+            sys.exit(f'invalid task name: {t}')
+
     train_path = os.path.join(root, 'train.txt')
     dev_path = os.path.join(root, 'dev.txt')
     test_path = os.path.join(root, 'test.txt')
@@ -121,46 +155,68 @@ def main(args):
     ############
     # NER
     ############
+    if 'ner' in tasks:
+        # load
+        train_data = load_ner(train_path)
+        dev_data = load_ner(dev_path)
+        test_data = load_ner(test_path)
+        logger.info('Loaded {} NER train samples'.format(len(train_data)))
+        logger.info('Loaded {} NER dev samples'.format(len(dev_data)))
+        logger.info('Loaded {} NER test samples'.format(len(test_data)))
 
-    # load
-    train_data = load_ner(train_path)
-    dev_data = load_ner(dev_path)
-    test_data = load_ner(test_path)
-    logger.info('Loaded {} NER train samples'.format(len(train_data)))
-    logger.info('Loaded {} NER dev samples'.format(len(dev_data)))
-    logger.info('Loaded {} NER test samples'.format(len(test_data)))
+        # build
+        train_fout = os.path.join(root, 'ner_train.json')
+        dev_fout = os.path.join(root, 'ner_dev.json')
+        test_fout = os.path.join(root, 'ner_test.json')
 
-    # build
-    train_fout = os.path.join(root, 'ner_train.json')
-    dev_fout = os.path.join(root, 'ner_dev.json')
-    test_fout = os.path.join(root, 'ner_test.json')
+        build_data(train_data, train_fout, tokenizer, NERLabelMapper, args.max_seq_len)
+        build_data(dev_data, dev_fout, tokenizer, NERLabelMapper, args.max_seq_len)
+        build_data(test_data, test_fout, tokenizer, NERLabelMapper, args.max_seq_len)
+        logger.info('done with NER')
 
-    build_data(train_data, train_fout, tokenizer, NERLabelMapper, args.max_seq_len)
-    build_data(dev_data, dev_fout, tokenizer, NERLabelMapper, args.max_seq_len)
-    build_data(test_data, test_fout, tokenizer, NERLabelMapper, args.max_seq_len)
-    logger.info('done with NER')
+    ############
+    # NER ALL CLASS
+    ############
+    if 'ner_ene' in tasks:
+        # load
+        train_data = load_ner_ene(train_path)
+        dev_data = load_ner_ene(dev_path)
+        test_data = load_ner_ene(test_path)
+        logger.info('Loaded {} NERENE train samples'.format(len(train_data)))
+        logger.info('Loaded {} NERENE dev samples'.format(len(dev_data)))
+        logger.info('Loaded {} NERENE test samples'.format(len(test_data)))
+
+        # build
+        train_fout = os.path.join(root, 'ner_ene_train.json')
+        dev_fout = os.path.join(root, 'ner_ene_dev.json')
+        test_fout = os.path.join(root, 'ner_ene_test.json')
+
+        build_data(train_data, train_fout, tokenizer, NERENELabelMapper, args.max_seq_len)
+        build_data(dev_data, dev_fout, tokenizer, NERENELabelMapper, args.max_seq_len)
+        build_data(test_data, test_fout, tokenizer, NERENELabelMapper, args.max_seq_len)
+        logger.info('done with NERENE')
 
     ############
     # POS Tagging
     ############
+    if 'pos' in tasks:
+        # load
+        train_data = load_pos(train_path)
+        dev_data = load_pos(dev_path)
+        test_data = load_pos(test_path)
+        logger.info('Loaded {} POS train samples'.format(len(train_data)))
+        logger.info('Loaded {} POS dev samples'.format(len(dev_data)))
+        logger.info('Loaded {} POS test samples'.format(len(test_data)))
 
-    # load
-    train_data = load_pos(train_path)
-    dev_data = load_pos(dev_path)
-    test_data = load_pos(test_path)
-    logger.info('Loaded {} POS train samples'.format(len(train_data)))
-    logger.info('Loaded {} POS dev samples'.format(len(dev_data)))
-    logger.info('Loaded {} POS test samples'.format(len(test_data)))
+        # build
+        train_fout = os.path.join(root, 'pos_train.json')
+        dev_fout = os.path.join(root, 'pos_dev.json')
+        test_fout = os.path.join(root, 'pos_test.json')
 
-    # build
-    train_fout = os.path.join(root, 'pos_train.json')
-    dev_fout = os.path.join(root, 'pos_dev.json')
-    test_fout = os.path.join(root, 'pos_test.json')
-
-    build_data(train_data, train_fout, tokenizer, POSLabelMapper, args.max_seq_len)
-    build_data(dev_data, dev_fout, tokenizer, POSLabelMapper, args.max_seq_len)
-    build_data(test_data, test_fout, tokenizer, POSLabelMapper, args.max_seq_len)
-    logger.info('done with POSTagging')
+        build_data(train_data, train_fout, tokenizer, POSLabelMapper, args.max_seq_len)
+        build_data(dev_data, dev_fout, tokenizer, POSLabelMapper, args.max_seq_len)
+        build_data(test_data, test_fout, tokenizer, POSLabelMapper, args.max_seq_len)
+        logger.info('done with POSTagging')
 
 
 if __name__ == "__main__":
