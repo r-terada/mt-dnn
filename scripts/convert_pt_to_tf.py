154927f510122d531d46d1d8007157e270db52df
r-terada <r.terada1993@gmail.com>
2019-08-21 04:27:12 +0900

    conver pt output layer to tf graph

--- scripts/convert_pt_to_tf.py
+++ scripts/convert_pt_to_tf.py
@@ -1,13 +1,41 @@
+'''
+pip install tensorflow onnx onnx-tf
+'''
 import argparse
 
 import os
+import warnings
+import onnx
 import torch
 import numpy as np
 import tensorflow as tf
+
+from typing import List, Tuple
+
+from onnx_tf.backend import prepare
 from pytorch_pretrained_bert import BertModel
 
+warnings.filterwarnings("default", category=UserWarning)
+warnings.filterwarnings("default", category=FutureWarning)
+warnings.filterwarnings("default", category=DeprecationWarning)
 
-def convert_pytorch_checkpoint_to_tf(model:BertModel, save_path:str):
+
+class SimpleNN(torch.nn.Module):
+
+    def __init__(self, output_shapes: List[Tuple[int, int]]):
+        super(SimpleNN, self).__init__()
+        self.scoring_list = torch.nn.ModuleList()
+        for (out, hidden) in output_shapes:
+            self.scoring_list.append(torch.nn.Linear(hidden, out))
+
+    def forward(self, X):
+        outs = []
+        for fc in self.scoring_list:
+            outs.append(fc(X))
+        return outs
+
+
+def convert_pytorch_checkpoint_to_tf(model:BertModel, save_dir:str):
 
     """
     :param model:BertModel Pytorch model instance to be converted
@@ -42,7 +70,6 @@ def convert_pytorch_checkpoint_to_tf(model:BertModel, save_path:str):
         ('weight', 'kernel')
     )
 
-    save_dir = os.path.dirname(save_path)
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
 
@@ -73,13 +100,24 @@ def convert_pytorch_checkpoint_to_tf(model:BertModel, save_path:str):
             print("Successfully created {}: {}".format(tf_name, np.allclose(tf_weight, torch_tensor)))
 
         saver = tf.train.Saver(tf.trainable_variables())
-        saver.save(session, save_path)
+        saver.save(session, os.path.join(save_dir, 'bert_model.ckpt'))
+
+
+def load_pb(path_to_pb):
+    with tf.gfile.GFile(path_to_pb, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def, name='')
+        return graph
 
 
 def main(args):
     state_dict = torch.load(args.pytorch_model_path)
+
+    # save fine-tuned bert weights
     bert_state_keys = list(filter(lambda x: x.startswith('bert'), state_dict['state'].keys()))
-    bert_state_dict = {k: v for k, v in state_dict.items() if k in bert_state_keys}
+    bert_state_dict = {k: v for k, v in state_dict['state'].items() if k in bert_state_keys}
 
     model = BertModel.from_pretrained(
         pretrained_model_name_or_path=args.bert_dir,
@@ -88,8 +126,42 @@ def main(args):
 
     convert_pytorch_checkpoint_to_tf(
         model=model,
-        save_path=args.tf_model_path
+        save_dir=args.tf_model_dir
     )
+
+    # save output layers
+    output_state_dict = {k: v for k, v in state_dict['state'].items() if k not in bert_state_keys}
+    shapes = []
+    for k, v in output_state_dict.items():
+        if k.endswith('weight'):
+            shapes.append(output_state_dict[k].shape)
+    model = SimpleNN(shapes)
+    model.load_state_dict(output_state_dict)
+
+    dummy_input = torch.from_numpy(np.ones(shapes[0][1]).reshape(1, -1)).float()
+    dummy_output = model(dummy_input)
+    print(dummy_output)
+
+
+    torch.onnx.export(
+        model,
+        dummy_input,
+        os.path.join(args.tf_model_dir, 'output_layer.onnx'),
+        input_names=['input'],
+        output_names=['output']
+    )
+
+    model_onnx = onnx.load(os.path.join(args.tf_model_dir, 'output_layer.onnx'))
+    tf_rep = prepare(model_onnx)
+    tf_rep.export_graph(os.path.join(args.tf_model_dir, 'output_layer.pb'))
+
+    # check saved tf model
+    tf_graph = load_pb(os.path.join(args.tf_model_dir, 'output_layer.pb'))
+    sess = tf.Session(graph=tf_graph)
+    output_tensor = tf_graph.get_tensor_by_name('add:0')
+    input_tensor = tf_graph.get_tensor_by_name('input:0')
+    output = sess.run(output_tensor, feed_dict={input_tensor: dummy_input})
+    print(output)
 
 
 if __name__ == "__main__":
@@ -103,9 +175,9 @@ if __name__ == "__main__":
                         type=str,
                         required=True,
                         help="/path/to/<pytorch-model-name>.pt")
-    parser.add_argument("--tf_model_path",
+    parser.add_argument("--tf_model_dir",
                         type=str,
                         required=True,
-                        help="file path to save tensorflow model")
+                        help="dir path to save tensorflow model")
     args = parser.parse_args()
     main(args)
